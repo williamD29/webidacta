1977b089136d5761f8807f20cfd2e715d84582cc
williamD29 <william.donval@gmail.com>
2020-05-01 23:25:58 +0200

    Global config tailwindcss/ui

---
+++ resources/tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+            }
+        }
+    },
+    variants: {},
+    plugins: [require('@tailwindcss/ui'), require('@tailwindcss/custom-forms')]
+}
